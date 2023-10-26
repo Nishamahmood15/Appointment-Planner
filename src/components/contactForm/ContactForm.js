9cf2baf500edd82816e65c2e71cbda34aa60baf5
MARehman75 <rehman.hanif16@gmail.com>
2023-10-26 17:35:18 +0500

    MARehman-1

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,52 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          <input
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Contact Name"
+          />
+        </label>
+        <br />
+
+        <label>
+          <input
+            name="phone"
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            required
+            placeholder="Phone Number"
+            pattern="^07([\d]{3})[(\D\s)]?[\d]{3}[(\D\s)]?[\d]{3}$"
+          />
+        </label>
+        <br />
+
+        <label>
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="Email Address"
+          />
+        </label>
+        <br />
+
+        <input
+          type="submit"
+          value="Add Contact"
+        />
+      </form>
+    </>
   );
 };
 
